0236de12e73419429d0d832366ef2dc7302db2db
Ben Smith <bens15351@gmail.com>
2026-02-09 21:05:48 +0000

    Moved v1 left firmware

--- Firmware/keyboard_left_firmware/Debug/objects.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (12.3.rel1)
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l:USBPDCORE_PD3_FULL_CM0PLUS_wc32.a
-
